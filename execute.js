0415703ec067531cfd599da82fd5480bef6f2735
pastak <pasta0915@gmail.com>
2013-04-20 18:20:50 +0900

    fix bug

---
+++ execute.js
@@ -0,0 +1,5 @@
+userdata={
+	token : document.getElementById('token'),
+	token_s : document.getElementById('token_s')
+}
+console.log(userdata);
---
+++ main.js
@@ -0,0 +1,119 @@
+function getinfo(){
+chrome.windows.getCurrent(function(_window){
+chrome.tabs.getSelected(_window.id, function(tab){
+		var url=tab.url;
+		var search=url.split("?")[1];
+		var ret=search.split("&");
+		var get = new Object();
+
+		for(var i = 0; i < ret.length; i++) {
+			var r = ret[i].split("=");
+			get[r[0]] = r[1];
+		}
+		document.getElementById("token").value=get["token"];
+		document.getElementById("token_s").value=get["token_s"];
+	});
+	});
+}
+function setting(){
+	var t=("'"+document.getElementById("token").value+"'");
+	var s=("'"+document.getElementById("token_s").value+"'");
+	var Config = {
+		token:t,
+		secret:s
+	};
+	localStorage.BlowsingNowConfigData = JSON.stringify(Config);
+	setcheck();
+}
+function setcheck(){
+	if(localStorage.BlowsingNowConfigData){
+   		document.getElementById("oauth").style.display="none";
+   		document.getElementById("set").style.display="block";
+	}else{
+   		document.getElementById("oauth").style.display="none";
+   		document.getElementById("setmiss").style.display="block";
+	}
+}
+function open_oauth(){
+chrome.windows.getCurrent(function(_window){
+chrome.tabs.getSelected(_window.id, function(tab){
+	chrome.tabs.create({
+		index:tab.index+1,
+		url:'http://pastak.cosmio.net/browsing_now_chrome/',
+		selected:true
+	});
+});
+});
+}
+function blowsingnow(){
+	document.getElementById("prompt").style.display="none";
+	document.getElementById("sending").style.display="block";
+	chrome.windows.getCurrent(function(_window){
+		chrome.tabs.getSelected(_window.id, function(tab){
+			console.log('aaa');
+			var Config = JSON.parse(localStorage.BlowsingNowConfigData);
+		  	var url=tab.url;
+		  	var title=tab.title;
+		  	var token=Config['token'].replace(/\'/g,"");
+		  	var secret=Config['secret'].replace(/\'/g,"");
+		  	var text=document.getElementById('userinput').value;
+		  	var req=("http://pastak.cosmio.net/browsing_now_chrome/submit.php"+"?url="+encodeURIComponent(url)+"&title="+encodeURIComponent(title)+"&token="+token+"&secret="+secret+"&text="+encodeURIComponent(text));
+		    console.log(req);
+		    var xhr=new XMLHttpRequest();
+		    xhr.open("get",req,false);
+		    xhr.onreadystatechange=function(){
+		    	if(xhr.readyState==4){
+		    		if(xhr.status == 200){
+			    		document.getElementById("sending").style.display="none";
+			    		document.getElementById("success").style.display="block";
+			    	}else{
+			    		document.getElementById("sending").style.display="none";
+			    		document.getElementById("error").style.display="block";
+			    	}
+		    	};
+		    };
+		    xhr.send(null);
+		});
+	});
+}
+
+function load(){
+	
+	if(localStorage.BlowsingNowConfigData){
+		document.body.style.width="450px";
+		document.getElementById("prompt").style.display="block";
+		document.getElementById("userinput").focus();
+		preview();
+	}else{
+		document.body.style.width="400px";
+		document.getElementById("oauth").style.display="block";
+	};
+}
+function preview(){
+chrome.windows.getCurrent(function(_window){
+	chrome.tabs.getSelected(_window.id, function(tab){
+		var u=document.getElementById("userinput").value;
+		if(u===''){
+		u='Now browsing';
+		}
+		document.getElementById("preview").textContent=(u+' : '+tab.title+" "+tab.url);
+	})
+})
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {
+	document.getElementById('form').addEventListener('submit',function(){
+		console.log('aa');
+		blowsingnow();
+		//return false;
+	})
+	document.getElementById('sendButton').addEventListener('click',blowsingnow);
+	document.getElementById('userinput').addEventListener('keydown',preview);
+	document.getElementById('userinput').addEventListener('keypress',preview);
+	document.getElementById('jumpOAuth').addEventListener('click',open_oauth);
+	document.getElementById('getInfo').addEventListener('click',getinfo);
+	document.getElementById('setting').addEventListener('click',setting);
+
+	load();
+});
